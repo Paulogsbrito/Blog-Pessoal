bd1438f092bbb724e314e9f1c828debccaf30455
Paulo <paulogsbrito@gmail.com>
2026-05-26 21:09:44 -0300

    realizada pequena alteração

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.data.repository.query.Param;
-
 
 import com.generation.blogpessoal.model.Postagem;
 
